a41abf655f450778ba7a88ce2f59bea967b28686
Mateus-msj3 <mateussouzaads@outlook.com>
2024-09-12 20:36:14 -0300

    first commit

---
+++ src/main/java/io/github/msj/revisionjpa/RevisionJpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.msj.revisionjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/msj/revisionjpa/RevisionJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.msj.revisionjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
